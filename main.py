f8e87eacdd7464b4c5faad1cd6134f047d601e07
Sam <samjc.yang@wpgholdings.com>
2024-01-20 23:34:55 +0800

    feat: 能不斷重新整理直到有票釋出馬上搶下來

--- main.py
+++ main.py
@@ -9,20 +9,22 @@ from selenium.webdriver.support import expected_conditions as EC
 driver = webdriver.Chrome();
 # 前往要去的網站
 driver.get('https://tixcraft.com/activity/detail/24_ive')
+# https://tixcraft.com/activity/detail/24_rod
+# https://tixcraft.com/activity/detail/24_ive
 
 driver.maximize_window()
 
 # 使用 WebDriverWait 等待畫面載入完成按下會員登入按鈕
-loginBtn = WebDriverWait(driver, 10).until(
+login_btn = WebDriverWait(driver, 10).until(
     EC.element_to_be_clickable((By.XPATH, '//*[@id="bs-navbar"]/div/div[2]/ul[3]/li/a'))
 )
-driver.execute_script("arguments[0].click();", loginBtn)
+driver.execute_script("arguments[0].click();", login_btn)
 
 # 使用 Facebook 登入
-loginBtn = WebDriverWait(driver, 10).until(
+login_btn = WebDriverWait(driver, 10).until(
     EC.element_to_be_clickable((By.XPATH, '//*[@id="loginFacebook"]'))
 )
-driver.execute_script("arguments[0].click();", loginBtn)
+driver.execute_script("arguments[0].click();", login_btn)
 
 # 找到輸入框並輸入帳號密碼
 username_box = WebDriverWait(driver, 10).until(
@@ -44,35 +46,34 @@ login_button = WebDriverWait(driver, 10).until(
 login_button.click()
 
 # 按下立即購票按鈕
-buyTicketBtn = WebDriverWait(driver, 10).until(
+buy_ticket_btn = WebDriverWait(driver, 10).until(
     EC.element_to_be_clickable((By.XPATH, '//*[@id="tab-func"]/li[1]/a'))
 )
-driver.execute_script("arguments[0].click();", buyTicketBtn)
+driver.execute_script("arguments[0].click();", buy_ticket_btn)
 
 # 按下立即訂購按鈕
-placeOrderBtn = WebDriverWait(driver, 10).until(
+place_order_btn = WebDriverWait(driver, 10).until(
     EC.element_to_be_clickable((By.XPATH, '//*[@id="gameList"]/table/tbody/tr[1]/td[4]/button'))
 )
-driver.execute_script("arguments[0].click();", placeOrderBtn)
+driver.execute_script("arguments[0].click();", place_order_btn)
 
-seats = driver.find_elements(By.XPATH, '//*[@id="selectseat"]/div/div[2]')
+while True:
+    try:
+        # 使用 XPath 來定位到包住所有位置的 div
+        all_seats_div = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="selectseat"]/div/div[2]/div[2]'))
+        )
 
-time.sleep(10)
+        seats = all_seats_div.find_elements(By.TAG_NAME, 'a')
 
-# while True:
-#     try:
-#         # 假設你的 div 有一個特定的 CSS class
-#         div = driver.find_element(By.CSS_SELECTOR, '#selectseat > div > div.area_select.col-lg-5.col-md-5.col-sm-5.col-xs-12.col-12.mgt-32.line-lf')
-#
-#         # 獲取 div 內的所有元素
-#         elements = div.find_elements(".//*")
-#
-#         # 尋訪每一個元素
-#         # for element in elements:
-#         #     # 如果元素是 enabled，那麼就 click 它
-#         #     if element.is_enabled():
-#         #         element.click()
-#         #         break
-#     except NoSuchElementException:
-#         # 如果沒有找到 div 或元素，那麼就繼續 loop
-#         continue
+        if seats:
+            firstSeat = seats[0]
+            # firstSeat.click()
+            driver.execute_script("arguments[0].click();", firstSeat)
+            time.sleep(10)
+            break
+        else:
+            print("找不到 <a> 標籤，將在一段時間後重新尋訪。")
+            driver.refresh()  # 刷新頁面
+    except NoSuchElementException:
+        print("發生意外，終止程式")
